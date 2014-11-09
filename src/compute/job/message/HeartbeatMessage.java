c750db004289ed40f509a36d3c1e520be1256fac
Little Leo <c2016.tw@gmail.com>
2014-11-09 16:19:16 -0500

    add HeartbeatMessage

---
+++ src/compute/job/message/HeartbeatMessage.java
@@ -0,0 +1,18 @@
+package compute.job.message;
+
+import java.io.Serializable;
+
+public class HeartbeatMessage implements Serializable{
+  public int numOfMapperSlot;
+  public int numOfReducerSlot;
+  public HeartbeatMessage(int numOfMapperSlot, int numOfReducerSlot){
+    this.numOfMapperSlot = numOfMapperSlot;
+    this.numOfReducerSlot = numOfReducerSlot;
+  }
+  public int getNumOfMapperSlot(){
+    return numOfMapperSlot;
+  }
+  public int getNumOfReducerSlot(){
+    return numOfReducerSlot;
+  }
+}
