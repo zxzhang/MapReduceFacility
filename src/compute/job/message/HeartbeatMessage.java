bc29f58506e690ffa6ffd4dff1be9414ebde2ab6
Little Leo <c2016.tw@gmail.com>
2014-11-09 18:10:55 -0500

    update message

--- src/compute/job/message/HeartbeatMessage.java
+++ src/compute/job/message/HeartbeatMessage.java
@@ -5,13 +5,16 @@ import java.io.Serializable;
 public class HeartbeatMessage implements Serializable{
   public int numOfMapperSlot;
   public int numOfReducerSlot;
+  
   public HeartbeatMessage(int numOfMapperSlot, int numOfReducerSlot){
     this.numOfMapperSlot = numOfMapperSlot;
     this.numOfReducerSlot = numOfReducerSlot;
   }
+  
   public int getNumOfMapperSlot(){
     return numOfMapperSlot;
   }
+  
   public int getNumOfReducerSlot(){
     return numOfReducerSlot;
   }
--- src/compute/job/message/JobTrackMessage.java
+++ src/compute/job/message/JobTrackMessage.java
@@ -1,12 +1,17 @@
 package compute.job.message;
 
+import java.io.Serializable;
+
 import compute.job.Job;
 import compute.job.JobStatus;
 
 
-public class JobTrackMessage {
+public class JobTrackMessage implements Serializable {
   JobStatus jobStatus;
   String jobId;
+  public JobStatus getJobStatus(){
+    return jobStatus;
+  }
   public JobTrackMessage(JobStatus jobStatus, String jobId){
     this.jobStatus = jobStatus;
     this.jobId = jobId;
@@ -14,5 +19,8 @@ public class JobTrackMessage {
   public JobTrackMessage(Job job){
     this.jobId = job.getJobId();
     this.jobStatus = job.getJobStatus();
+  }
+  public String toString(){
+    return String.format("[%s]-[%s]", jobId, jobStatus.toString());
   }
 }
