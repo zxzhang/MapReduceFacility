d2c6ce11f71538880e20037627c7945da628162c
Little Leo <c2016.tw@gmail.com>
2014-11-10 18:10:12 -0500

    add maxNumOfReducePreprocess

--- src/compute/configure/TaskTrackerConfiguration.java
+++ src/compute/configure/TaskTrackerConfiguration.java
@@ -3,4 +3,5 @@ package compute.configure;
 public class TaskTrackerConfiguration {
   public static int maxNumOfMapper = 2;
   public static int maxNumOfReducer = 2;
+  public static int maxNumOfReducePreprocess = 2;
 }
