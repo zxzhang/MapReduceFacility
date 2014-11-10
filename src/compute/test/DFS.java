1518a6a170a0aa405a7f90df9755c49c7fcccade
Little Leo <c2016.tw@gmail.com>
2014-11-10 18:09:47 -0500

    add getHost

--- src/compute/test/DFS.java
+++ src/compute/test/DFS.java
@@ -2,13 +2,15 @@ package compute.test;
 
 import java.util.List;
 
+import compute.utility.Host;
+
 
 
 public abstract class DFS {
   
-  
   public abstract DFSReader getReader(String dfsPath) throws Exception;
   public abstract DFSWriter getWriter(String dfsPath) throws Exception;
   public abstract List<String> ls(String dfsDirPath);
+  public abstract Host getHost(String dfsPath, int version);
   
 }
